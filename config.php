d6b3d5b75777ddce19ccd4c629be53fa7d2a2edc
Marvin Ludwig <MarvinLudwig@users.noreply.github.com>
2015-06-21 18:04:34 +0200

    Update config.php

--- config.php
+++ config.php
@@ -1,4 +1,7 @@
 <?php
+// Repository: https://github.com/MarvinLudwig/EPC069-12_for_Billomat
+// Licence: Apache 2.0
+// Version 1.0
 
 // Must be adjusted:
 
@@ -7,16 +10,17 @@ define("USER_ID", ""); // your Billomat user ID
 
 // Can be adjusted:
 
+define("LANG","de"); // language
 define("PAYMENT_REFERENCE_INVOICE", "invoice_number, label"); // invoice information that is used as payment reference
 define("PAYMENT_REFERENCE_CLIENT", "client_number, name"); // client information that is used as payment reference
 define("APP_ID", ""); // Raise the 15 minute limit from 300 to 1000 calls: https://{YOUR_USER_ID}.billomat.net/portal/apps
 define("APP_SECRET", "");
-define("DEBUG_LEVEL", 1); // 0: no debug, 1: only PHP errors, 2: PHP errors and curl results
+define("DEBUG_LEVEL", 0); // 0: no debug, 1: only PHP errors, 2: PHP errors and curl results
 define("QR_FILENAME", "GiroCode.png"); // filename of the email attachment
 define("CUT_PAYMENT_REFERENCE", "0"); // there is maximum payload of 331 bytes for the qr data
-                                       // 1: the payment reference text is cut automatically to meet the limit of 331 bytes
-									   // 0: you are asked every time, before the text is cut
-									   // note: usually the 331 bytes limit should not be a problem, only if a lot of unicode characters are used 
+                                      	// 1: the payment reference text is cut automatically to meet the limit of 331 bytes
+					// 0: you are asked every time, before the text is cut
+					// note: usually the 331 bytes limit should not be a problem, only if a lot of unicode characters are used 
 
 // Do not touch:
 
@@ -27,5 +31,4 @@ if (DEBUG_LEVEL > 1){
 }
 
 require("utils.php");
-
-?>
+require("lang_".LANG.".php");
