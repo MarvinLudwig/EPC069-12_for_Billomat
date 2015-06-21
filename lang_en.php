df2d1509e64e67a7a36840c6ce12ee7d33242fa4
Marvin Ludwig <MarvinLudwig@users.noreply.github.com>
2015-06-21 18:05:33 +0200

    Create lang_en.php

---
+++ lang_en.php
@@ -0,0 +1,23 @@
+<?php
+// Repository: https://github.com/MarvinLudwig/EPC069-12_for_Billomat
+// Licence: Apache 2.0
+// Version 1.0
+
+function msg($id, $params = null){
+	switch ($id){
+		case 'inv_no' : return 'Inv #'; 
+		case 'date' : return 'Date';
+		case 'client_no' : return 'Client #';
+		case 'name_city' : return 'Name & City';
+		case 'email_invalid' : return 'Email address is invalid.';
+		case 'send_all_invoices' : return 'Send all selected invoices';
+		case 'show' : return 'show';
+		case 'send' : return 'send';
+		case 'show_qr' : return 'Show QR-Code for invoice '.$params['invoice_number'];
+		case 'send_mail' : return 'Send invoice '.$params['invoice_number'];
+		case 'cut_text' : return 'We can cut the text, it would look like that:';
+		case 'cut_text_shall_we' : return 'Shall we?';
+	}
+}
+
+?>
